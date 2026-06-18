2ababf62357c75b74dc2a968e178a3d765ad2586
Arsh Tiwari <tiwariarsh348@gmail.com>
2026-06-18 18:07:14 +0530

    Make product of the number

--- second.py
+++ second.py
@@ -1,5 +1,5 @@
 # to find the multiplecation of the 2 number
-a = int(input("enter your nunber :-"))
-b = int(input("enter your nunber :-"))
+a = int(input("enter your nunber :- "))
+b = int(input("enter your nunber :- "))
 c = a*b
-print(f"the product of the two number{c}")
+print(f"the product of the two number = {c}")
